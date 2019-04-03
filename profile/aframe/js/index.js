4be9737c1ed1f1b923a61e7a317dd1a0d90ce5a2
Philippe Coval <p.coval@samsung.com>
2019-04-03 19:33:58 +0200

    aframe: Improve rendering on dome
    Change-Id: I5351f2ebfcef075d25d6a8057296fb01f19916e8
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- profile/aframe/js/index.js
+++ profile/aframe/js/index.js
@@ -31,27 +31,149 @@ viewer.log = !console.log || function(text) {
   }
 };
 
+// maybe removed
+viewer.startUpdateProperty = function(model, name, view) {
+  if (model) {
+    return;
+  } // TODO
+  const property = model.properties[name];
+  const endpoint = property.links[0].href;
+  const type = property.type;
+  const el = view.children[0];
+  app.get(endpoint, function(err, data) {
+    if (!err) {
+      let text = view.getAttribute('text', 'value').value;
+      text = `\n${text}\n${data})`;
+      view.setAttribute('text', 'value', text);
+      let value = JSON.parse(data)[name];
+
+      switch (type) {
+        case 'boolean':
+          try {
+            value = (value) ? 1 : 0;
+            el.setAttribute('ui-toggle', 'value', value);
+          // el.emit('change', {value: value});
+          } catch (e) {
+            console.error(`error: ${e}`);
+          }
+          break;
+
+        case 'number':
+        case 'integer':
+          el.setAttribute('ui-slider', 'value', value);
+          break;
+
+        case 'string':
+          this.log(data);
+          if (semType === 'ColorProperty' || name === 'color') { // TODO
+            el.setAttribute('ui-button', 'color', value);
+          } else {
+            el.setAttribute('ui-rotary', 'value', value.length);
+          }
+          break;
+        default:
+          self.log('TODO:');
+      }
+    }
+  });
+};
+
+
+// TO relocate a-frame-io-widget.js ?
+viewer.createPropertyElement = function(model, name) {
+  const that = this;
+  const property = model.properties[name];
+  const type = property.type;
+  const semType = property['@type'];
+  let el = null;
+  const endpoint = `${model.links[0].href}/${name}`;
+  const view = document.createElement('a-text');
+  const suffix = (property.title) ? `:\n(${property.title})` : '';
+  view.setAttribute('value',
+                    `\n${model.name}${suffix}`);
+  view.setAttribute('color',
+                    (property.readOnly) ? '#FFA0A0' : '#A0FFA0');
+  view.setAttribute('width', 1);
+  view.setAttribute('align', 'center');
+  const id = `${this.count++}`;
+  that.log(`createPropertyElement: ${type}/${semType}`);
+  switch (type) {
+    case 'boolean':
+      el = document.createElement('a-entity');
+      el.setAttribute('rotation', '90 0 0');
+      el.setAttribute('scale', '.8 .8 .8');
+      el.setAttribute('ui-toggle', 'value', 0);
+      break;
+    case 'number':
+    case 'integer':
+      el = document.createElement('a-entity');
+      el.setAttribute('rotation', '90 0 0');
+      el.setAttribute('scale', '.8 .8 .8');
+      el.setAttribute('ui-slider', 'value', 0);
+      el.setAttribute('ui-slider', 'min', 0);
+      el.setAttribute('ui-slider', 'max', 100); // TODO
+      break;
+    case 'string':
+      if (semType === 'ColorProperty' || name === 'color') { // TODO
+        el = document.createElement('a-entity');
+        el.setAttribute('ui-button', 'size', '0.1');
+        el.setAttribute('rotation', '90 0 0');
+        el.setAttribute('scale', '.8 .8 .8');
+      } else {
+        el = document.createElement('a-entity');
+        el.setAttribute('ui-rotary', 'value', 0); // TODO: updateSchema
+        el.setAttribute('rotation', '90 0 0');
+        el.setAttribute('scale', '.8 .8 .8');
+      }
+      break;
+    default:
+      that.log(`TODO: ${type}`);
+      el = document.createElement('a-box');
+      el.setAttribute('scale', '.2 .2 .2');
+      el.setAttribute('radius', '0.1');
+      el.setAttribute('color', '#BADC0D');
+  }
+  el.setAttribute('position', '0 0.2 0');
+  el.setAttribute('id', `widget-${id}`);
+  el.addEventListener('change', function(e) {
+    if (e.detail && !property.readOnly) {
+      const payload = {};
+      payload[name] = !!(e.detail.value !== 0);
+      app.put(endpoint, payload, function(res, data) {
+        if (res) {
+          console.error(data);
+        }
+      });
+    } else {
+      that.startUpdateProperty(model, name, view);
+    }
+  });
+  view.setAttribute('id', `view-${id}`);
+  view.appendChild(el);
+
+  return view;
+};
+
 
 viewer.updateThingView = function(err, data, model) {
-  const self = this;
   if (err) {
     throw err;
   }
-  self.log(`updateThingView: ${data}`);
-  self.log(data);
-  self.log(model);
+  this.log(`updateThingView: ${data}`);
+  this.log(data);
+  this.log(model);
   for (const name in data) {
     const type = model.properties[name].type;
     const semType = model.properties[name]['@type'];
     const el = model.local[name].view.children[0];
-    self.log(`updateThingView/prop/${name}:${type}`);
+    this.log(`updateThingView/prop/${name}:${type}`);
     switch (type) { // TODO: mapping design pattern
       case 'boolean':
         el.setAttribute('ui-toggle', 'value', data[name] ? 1 : 0);
         break;
       case 'number':
       case 'integer':
-        self.log(`// TODO update in widget${data[name]}`);
+        this.log(`// TODO update in widget${data[name]}`);
         el.setAttribute('ui-slider', 'value', data[name]);
         break;
       case 'string':
@@ -62,18 +184,18 @@ viewer.updateThingView = function(err, data, model) {
         }
         break;
       default:
-        self.log(`TODO: callback: ${name} : ${type}`);
+        this.log(`TODO: callback: ${name} : ${type}`);
     }
   }
 };
 
 
 viewer.appendThing = function(model) {
-  const self = this;
+  const that = viewer;
   const view = null;
   let propertyName = null;
-  // this.log(`appendThing: ${model.type}`);
-  // this.log(model);
+  // that.log(`appendThing: ${model.type}`);
+  // that.log(model);
   model.local = {};
   for (propertyName in model.properties) {
     const el = this.createPropertyElement(model, propertyName);
@@ -82,18 +204,18 @@ viewer.appendThing = function(model) {
     } catch (err) {
       console.error(`ignore: ${err}`);
     }
-    el.object3D.rotateY(this.rotation[1]);
-    el.object3D.rotateX(this.rotation[0]);
+    el.object3D.rotateY(that.rotation[1]);
+    el.object3D.rotateX(that.rotation[0]);
     const step = 8;
     el.object3D.translateZ(-4);
-    this.rotation[1] += (2 * Math.PI / step) / Math.cos(this.rotation[0]);
+    this.rotation[1] += (2 * Math.PI / step) / Math.cos(that.rotation[0]);
 
     if (this.rotation[1] >= 2 * Math.PI) {
-      this.rotation[1] = 2 * Math.PI - this.rotation[1];
-      this.rotation[0] += 2 * Math.PI / 2 / 2 / step;
+      that.rotation[1] = 2 * Math.PI - that.rotation[1];
+      that.rotation[0] += 2 * Math.PI / 2 / 2 / step;
       // TODO : bottom
     }
-    if (Math.abs(this.rotation[0]) >=
+    if (Math.abs(that.rotation[0]) >=
         Math.ceil(2 * Math.PI / 2 / 2 / step) * step) {
       this.rotation[0] = 0;
     }
@@ -103,11 +225,11 @@ viewer.appendThing = function(model) {
     model.local[propertyName] = {view: el};
   }
 
-  this.poll(model, function(err, data) {
-    self.updateThingView(err, data, model);
+  app.poll(model, function(err, data) {
+    that.updateThingView(err, data, model);
   });
-  this.listenThing(model, function(err, data) {
-    self.updateThingView(err, data, model);
+  app.listenThing(model, function(err, data) {
+    that.updateThingView(err, data, model);
   });
 
   return view;
@@ -115,8 +237,7 @@ viewer.appendThing = function(model) {
 
 
 viewer.handleResponse = function(err, data) {
-  const self = viewer;
-  // self.log(`handleResponse: ${typeof data}`);
+  // this.log(`handleResponse: ${typeof data}`);
   if (err || !data) {
     console.error(err);
     throw err;
@@ -126,14 +247,13 @@ viewer.handleResponse = function(err, data) {
   if (typeof data === 'string' && data) {
     model = data && JSON.parse(data);
   }
-  // self.log(JSON.stringify(model));
   if (Array.isArray(model)) {
     let index;
     for (index = 0; index < model.length; index++) {
       viewer.handleResponse(err, model[index]);
     }
   } else {
-    self.appendThing(model);
+    this.appendThing(model);
   }
 };
 
@@ -142,7 +262,6 @@ viewer.query = function(endpoint) {
   if (!endpoint) {
     endpoint = localStorage.endpoint;
   }
-  // this.log(`log: query: ${endpoint}`);
   app.get(endpoint, viewer.handleResponse);
 };
 
